ae07826dc28f6eddc09dec3ef0a4705193a20eaf
Mariya <chegrenets@mail.ru>
2022-12-04 19:47:38 +0300

    Задача 15

---
+++ task15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите цифру, обозначающую день недели (от 1 до 7): ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>=6)
+{
+    Console.WriteLine("Да");
+}    
+else
+{
+    Console.WriteLine("Нет");    
+}
