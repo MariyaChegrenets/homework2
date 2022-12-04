3b09cfb28785c940a8e2dfd90ef8d00f46b99c26
Mariya <chegrenets@mail.ru>
2022-12-04 19:55:18 +0300

    Задача 16

---
+++ task16/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b == a * a)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
