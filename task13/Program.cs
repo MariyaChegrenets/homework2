813248b40ca45a3e407aea591e1e235d2d364be6
Mariya <chegrenets@mail.ru>
2022-12-04 21:13:52 +0300

    Задача 13

---
+++ task13/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+string a = Console.ReadLine();
+if(a.Length > 2)
+    Console.Write("Третья цифра числа:" + a[2]);
+else    
+    Console.Write("Третьей цифры нет");  
